f3d88f90cac28d82a31536bf70d7552fb151356b
Miras <esimkhanovmiras@gmail.com>
2026-04-27 04:57:49 +0600

    Исправлен баг с регистрацией

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
+
+User = get_user_model()
 
 
 class RegisterForm(UserCreationForm):
@@ -8,4 +11,4 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
